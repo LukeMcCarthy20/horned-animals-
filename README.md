a7134ffce136b71cb45d7ae55619d15c6268f39b
Luke McCarthy <astartofanewlight@gmail.com>
2023-10-18 14:22:19 -0500

    moved import data into app

--- README.md
+++ README.md
@@ -78,3 +78,15 @@ Start time:
 Finish time:
 
 Actual time needed to complete:
+
+---
+
+Number and name of feature:
+
+Estimate of time needed to complete: 2 hours
+
+Start time: 2:30
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,19 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import HornedAnimals from "./HornedAnimals.jsx";
-import data from "./data.json";
 
 
 class Gallery extends React.Component {
   render() {
-    console.log(data);
-    let animals = [];
-    data.forEach((newAnimal, index) => {
-      animals.push(<HornedAnimals   title={newAnimal.title} desc={newAnimal.description} img={newAnimal.image_url} key={index}/>)
-    })
+  let animals = this.props.data.map((newAnimal, index) => (
+   <HornedAnimals   
+   title={newAnimal.title}
+    desc={newAnimal.description} 
+    img={newAnimal.image_url} 
+    key={index}
+    />
+  ));
 
     return (
       <>
-        <main>{animals}</main>
+        <main>
+          
+          {animals}
+          
+          </main>
+          
       </>
     );
   }
